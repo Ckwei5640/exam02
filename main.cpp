e25d16ebc298345b91796e27ff381d0d7cde2f85
Ckwei <kkww0259@gmail.com>
2020-04-29 16:38:18 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include "mbed.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#include <cmath>
+#include <string>
+#define UINT14_MAX        16383
+// FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+// FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+
+I2C i2c( PTD9,PTD8);
+Serial pc(USBTX, USBRX);
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+EventQueue queue;
+//Thread thread1;
+Timeout timeout;
+DigitalIn sw3(SW3);
+DigitalOut led1(LED1);
+uint8_t who_am_i, data[2], res[6];
+int16_t acc16;
+float t[3];
+int distence = 0;
+int larger5 = 0;
+int tol_dis = 0;
+std::string log1[109];
+int i = 0;
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
+void FXOS8700CQ_writeRegs(uint8_t * data, int len);
+void Logger();
+void Blink();
+void PrintLogger();
+
+int main() {
+
+   pc.baud(115200);
+
+   led1 = 1;
+
+   // Enable the FXOS8700Q
+   FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+   data[1] |= 0x01;
+   data[0] = FXOS8700Q_CTRL_REG1;
+   FXOS8700CQ_writeRegs(data, 2);
+
+   // Get the slave address
+   FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
+   
+   pc.printf("Here is %x\r\n", who_am_i);
+
+   while (true) {
+        if(sw3 == 0){
+            queue.call_every(100,&Logger);
+            queue.call_every(500,&Blink);
+            queue.call_in(10010,&PrintLogger);
+            timeout.attach(callback(&queue, &EventQueue::break_dispatch),10.2);
+            queue.dispatch();
+        }
+   }
+}
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+   char t = addr;
+   i2c.write(m_addr, &t, 1, true);
+   i2c.read(m_addr, (char *)data, len);
+}
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+   i2c.write(m_addr, (char *)data, len);
+}
+
+void Logger(){
+      FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+      
+      acc16 = (res[0] << 6) | (res[1] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[0] = ((float)acc16) / 4096.0f;
+
+      acc16 = (res[2] << 6) | (res[3] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[1] = ((float)acc16) / 4096.0f;
+      
+      acc16 = (res[4] << 6) | (res[5] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[2] = ((float)acc16) / 4096.0f;
+
+      // d = a*t^2/2  a = 9.8*t[i] d = 9.8*t[i]*t^2/2
+      distence = 9.8*t[1]/2;
+      if(tol_dis*distence >= 0){
+          tol_dis = tol_dis + distence;
+      }
+      else tol_dis = 0;
+
+      if(tol_dis > 5){
+          larger5 = 1;
+      }
+      else larger5 = 0;
+
+      /* printf("FXOS8700Q ACC: X=%1.4f(%x%x) Y=%1.4f(%x%x) Z=%1.4f(%x%x)\r\n",\
+            t[0], res[0], res[1],\
+            t[1], res[2], res[3],\
+            t[2], res[4], res[5]\
+      );*/
+      // wait(1.0);
+
+      log1[i] =std::to_string(t[0]) + " " + std::to_string(t[1]) +" " + std::to_string(t[2]) + " " + std::to_string(larger5) + "\r\n";
+      // to_string convert to string type
+      i++;  
+}
+
+void Blink(){
+    led1 = !led1;
+}
+
+void PrintLogger(){
+    for(int i = 0; i < 100; i++){
+        pc.printf("%s",log1[i].c_str());
+        //wait(0.01);
+    }
+
+}
